75ba4aeec011a579c516a6117eb9fad88a824a8e
masataka_iMac <cdehilmnrr@gmail.com>
2020-01-26 20:10:39 +0900

    ネットワーク修正

--- common/subnets.tf
+++ common/subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public_web1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.10.0/24"
+  cidr_block        = "20.0.10.0/24"
   availability_zone = "ap-northeast-1a"
   tags = {
     Name = "terraform_web1"
@@ -16,7 +16,7 @@ resource "aws_subnet" "public_web1" {
 # }
 resource "aws_subnet" "private_rds1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.20.0/24"
+  cidr_block        = "20.0.20.0/24"
   availability_zone = "ap-northeast-1a"
   tags = {
     Name = "terraform_rds1"
@@ -24,7 +24,7 @@ resource "aws_subnet" "private_rds1" {
 }
 resource "aws_subnet" "private_rds2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.21.0/24"
+  cidr_block        = "20.0.21.0/24"
   availability_zone = "ap-northeast-1c"
   tags = {
     Name = "terraform_rds2"
--- common/vpc.tf
+++ common/vpc.tf
@@ -1,6 +1,6 @@
 # VPC
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16" #クラスレスのIPアドレスの割当て
+  cidr_block       = "20.0.0.0/16" #クラスレスのIPアドレスの割当て
   instance_tenancy = "default"     #ハードウェアを共有する
   tags = {
     Name = "terraform_vpc"
